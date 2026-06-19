227ed2a8ea92b29ccca062335b1c76acd33b8e70
Bhanu Pratap Singh <68643355+Bylerma@users.noreply.github.com>
2026-06-19 10:21:08 +0530

    Add solution for 33. Search in Rotated Sorted Array

--- solutions/2026-06-19/0033-search-in-rotated-sorted-array/solution.py
+++ solutions/2026-06-19/0033-search-in-rotated-sorted-array/solution.py
@@ -5,22 +5,32 @@
 # Language: Python3
 # URL: https://leetcode.com/problems/search-in-rotated-sorted-array/
 # Date: 2026-06-19
-# Runtime: N/A
+# Runtime: 20260ms (Beats 100%)
 # Memory: N/A
 
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def search(self, nums: List[int], target: int) -> int:
         low = 0
         high = len(nums) - 1
 
         while low <= high:
             mid = low + (high - low) // 2
 
-            if nums[mid] > target:
-                high = mid - 1
-            elif nums[mid] < target:
-                low = mid + 1
-            else:
+            if nums[mid] == target:
                 return mid
-        
-        return low
+
+            if nums[low] <= nums[mid]:
+
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+
+            else:
+
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        return -1
