6191d455c07b14bb32641c2e5e2e5ab31ce0afc8
Bhanu Pratap Singh <68643355+Bylerma@users.noreply.github.com>
2026-07-18 01:23:17 +0530

    Add solution for 3867. Sum of GCD of Formed Pairs

---
+++ solutions/2026-07-18/3867-sum-of-gcd-of-formed-pairs/solution.py
@@ -0,0 +1,23 @@
+# Number: 3867
+# Title: Sum of GCD of Formed Pairs
+# Difficulty: Medium
+# Tags: Array, Math, Two Pointers, Sorting, Simulation, Number Theory
+# Language: Python3
+# URL: https://leetcode.com/problems/sum-of-gcd-of-formed-pairs/
+# Date: 2026-07-18
+# Runtime: 078ms
+# Memory: N/A
+
+from math import gcd
+class Solution:
+    def gcdSum(self, nums: list[int]) -> int:
+        gcdprefix = []
+        mx = -1
+        for i in nums:
+            mx = max(mx, i)
+            gcdprefix.append(gcd(i,mx))
+        gcdprefix.sort()
+        totalgcd = 0
+        for i in range(len(gcdprefix)//2):
+            totalgcd += gcd(gcdprefix[i], gcdprefix[-(i+1)])
+        return totalgcd
